092fdd60ec23f2034d0035214f8e7148dbc5b057
lumin <bats20082008@gmail.com>
2016-05-16 14:39:59 +0800

    test ok.

---
+++ ionic-todo/platforms/android/assets/www/js/todo-service.js
@@ -0,0 +1,84 @@
+﻿angular.module('todo.services', [])
+
+.factory('SQLservice', function ($q) {
+    var db;
+
+    function createDB() {
+        try{
+            db = window.openDatabase("todoDB", 1.0, "ToDoApp", 10*1024*1024);
+            db.transaction(function(tx){
+                tx.executeSql("create table if not exists tasks (task_id integer not null primary key autoincrement, \
+                task_name varchar(100))", []);
+            })
+        } catch (err) {
+            alert('Error processing SQL: ' + err);
+        }
+    }
+
+    function setTasks(tname) {
+        return promiseQuery("insert into tasks (task_name) values('" + tname + "')", defaultResultHandler,
+            defaultErrorHandler);
+    }
+
+    function delTasks(tid) {
+        return promiseQuery("delete from tasks where task_id = " + tid, defaultResultHandler,
+            defaultErrorHandler);
+    }
+
+    function updateTasks(tname, tid) {
+        return promiseQuery("update tasks set task_name = '" + tname + "' where task_id = " + tid, defaultResultHandler,
+            defaultErrorHandler);
+    }
+
+    function getTasks() {
+        return promiseQuery("select * from tasks", defaultResultHandler,
+            defaultErrorHandler);
+    }
+
+    function defaultResultHandler(deferred) {
+        return function (tx, results) {
+            var len = results.rows.length;
+            var output_results = [];
+
+            for (var i = 0; i < len; i++) {
+                var t = { 'task_id': results.rows.item(i).task_id, 'task_name': results.rows.item(i).task_name };
+                output_results.push(t);
+            }
+            deferred.resolve(output_results);
+        }
+    }
+
+    function defaultErrorHandler(deferred) {
+        return function (tx, results) {
+            var len = 0;
+            var output_results = '';
+            deferred.resolve(output_results);
+        }
+    }
+
+    function promiseQuery(query, successCB, errorCB) {
+        var deferred = $q.defer();
+        db.transaction(function (tx) {
+            tx.executeSql(query, [], successCB(deferred), errorCB(deferred));
+        }, errorCB);
+        return deferred.promise;
+    }
+
+    return {
+        setup: function () {
+            return createDB();
+        },
+        set: function (t_name) {
+            return setTasks(t_name);
+        },
+        del: function (taskid) {
+            return delTasks(taskid);
+        },
+        edit: function (t_name, taskid) {
+            return updateTasks(t_name, taskid);
+        },
+        all: function () {
+            return getTasks();
+        }
+    }
+})
--- ionic-todo/www/js/app.js
+++ ionic-todo/www/js/app.js
@@ -3,22 +3,96 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('todo', ['ionic', 'todo.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+/*
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (cordova.platformId === 'ios' && window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
+*/
+
+.controller('TodoCtrl', function ($scope, $ionicModal, $ionicPopup, SQLservice) {
+
+    SQLservice.setup();
+
+    $scope.loadTask = function () {
+        /*
+        $scope.tasks = [
+            { 'task_id': 1, 'task_name': 'task 1' },
+            { 'task_id': 2, 'task_name': 'task 2' },
+            { 'task_id': 3, 'task_name': 'task 3' }
+        ]
+        */
+        SQLservice.all().then(function (results) {
+            $scope.tasks = results;
+        })
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+
+    $scope.loadTask();
+
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+
+    $scope.newTask = function () {
+        $scope.taskModal.show();
     }
-  });
+
+    $scope.closeNewTask = function () {
+        $scope.taskModal.hide();
+    }
+
+    $scope.createTask = function (task) {
+        //save task
+        SQLservice.set(task.title);
+        $scope.loadTask();
+        $scope.taskModal.hide();
+        task.title = '';
+    }
+
+    $scope.onItemDelete = function (taskid) {
+        $ionicPopup.confirm({
+            title: 'Confirm Delete',
+            content: 'Are you sure you want to delete this task?'
+        }).then(function (res) {
+            if (res) {
+                //del task
+                SQLservice.del(taskid);
+                $scope.loadTask();
+            }
+        })
+    }
+
+    $scope.onItemEdit = function (taskid) {
+        $ionicPopup.prompt({
+            title: 'Update task',
+            sunTitle: 'Enter new task'
+        }).then(function (res) {
+            //edit task
+            SQLservice.edit(res, taskid);
+            $scope.loadTask();
+        })
+    }
+
+    $scope.moveItem = function (item, $fromIndex, $toIndex) {
+        $scope.tasks.splice($fromIndex - 1, 1);
+        $scope.tasks.splice($toIndex, 0, item);
+    }
 })
